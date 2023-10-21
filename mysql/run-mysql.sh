f78f5f554d24388e528df40b9d07be0bcc7afe78
Xinghua Zhang <xinghua24@gmail.com>
2023-10-21 00:53:30 -0400

    prevent port from access from public

--- mysql/run-mysql.sh
+++ mysql/run-mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --name spring-simple-forum-mysql \
   -e MYSQL_ROOT_PASSWORD=password \
-  -p 3306:3306 \
+  -p 127.0.0.1:3306:3306 \
   -d \
   -v ${PWD}/.mysql-data:/var/lib/mysql \
   mysql
